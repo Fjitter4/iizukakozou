e4a0f7ea8d905c5999cc1983d4ce9c79c6cfe80b
macbaito <pleiadesm7@gmail.com>
2020-08-19 12:26:44 +0900

    原点ソースコード

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+$(function() {
+    $(".colorboxlink").colorbox({
+        rel: 'colorbox',
+        maxWidth:"90%",
+        maxHeight:"90%",
+        opacity: 0.4
+    });
+});
